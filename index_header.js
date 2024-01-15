aef4c34bbcdf191bf7faa10ac5bd29949df110d3
na7na712 <inu10neko1201@gmail.com>
2024-01-15 14:27:43 +0900

    修正

--- index_header.js
+++ index_header.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
       <header>
           <nav>
               <ul>
-                  <li><a href="../index.html">TOP</a></li>
-                  <li><a href="../cardlist.html">カードリスト</a></li>
-                  <li><a href="../locationlist.html">ロケーションリスト</a></li>
-                  <li><a href="../spotlights_share.html">スポトレチェッカー</a></li>
+                  <li><a href="index.html">TOP</a></li>
+                  <li><a href="cardlist.html">カードリスト</a></li>
+                  <li><a href="locationlist.html">ロケーションリスト</a></li>
+                  <li><a href="spotlights_share.html">スポトレチェッカー</a></li>
                   <!-- 他のセクションへのリンクを追加 -->
               </ul>
           </nav>
