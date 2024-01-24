bf97cd6e883450186b30702609e6a48e19ed92c4
na7na712 <inu10neko1201@gmail.com>
2024-01-24 15:04:54 +0900

    お問い合わせ追加

--- index_header.js
+++ index_header.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
                   <li><a href="../locationlist.html">ロケーションリスト</a></li>
                   <li><a href="../spotlights_share.html">スポトレチェッカー</a></li>
                   <li><a href="../report.html">デッキ紹介</a></li>
+                  <li><a href="https://twitter.com/NANA1051815">お問い合わせはこちら</a></li>
                   <!-- 他のセクションへのリンクを追加 -->
               </ul>
           </nav>
--- locationlist_header.js
+++ locationlist_header.js
@@ -7,8 +7,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     <li><a href="index.html">TOP</a></li>
                     <li><a href="cardlist.html">カードリスト</a></li>
                     <li><a href="locationlist.html">ロケーションリスト</a></li>
-                    <li><a href="../spotlights_share.html">スポトレチェッカー</a></li>
-                    <li><a href="../report.html">デッキ紹介</a></li>
+                    <li><a href="spotlights_share.html">スポトレチェッカー</a></li>
+                    <li><a href="report.html">デッキ紹介</a></li>
+                    <li><a href="https://twitter.com/NANA1051815">お問い合わせはこちら</a></li>
                     <!-- 他のセクションへのリンクを追加 -->
                 </ul>
                 <ul>
