520ced96cb7d1dd7212e3a747763ef6d14cbada6
na7na712 <inu10neko1201@gmail.com>
2024-03-05 13:15:11 +0900

    スポトレ更新

--- analytics.js
+++ analytics.js
@@ -1,19 +1,24 @@
+// Google AdSenseの広告スクリプトを非同期で読み込む
+var adsenseScriptTag = document.createElement('script');
+adsenseScriptTag.async = true;
+adsenseScriptTag.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9769812822312738';
+document.head.appendChild(adsenseScriptTag);
 
 // 非同期でGoogle Tag Managerのスクリプトを読み込む
-var scriptTag = document.createElement('script');
-scriptTag.async = true;
-scriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=G-RCLT2WB2EF';
-document.head.appendChild(scriptTag);
+var gtagScriptTag = document.createElement('script');
+gtagScriptTag.async = true;
+gtagScriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=G-RCLT2WB2EF';
+document.head.appendChild(gtagScriptTag);
 
-// スクリプトが読み込まれた後に実行される関数
-scriptTag.onload = function() {
-// Google Tag Managerの初期化
-window.dataLayer = window.dataLayer || [];
+// Google Tag Managerのスクリプトが読み込まれた後に実行される関数
+gtagScriptTag.onload = function() {
+    // Google Tag Managerの初期化
+    window.dataLayer = window.dataLayer || [];
 
-function gtag() {
-  dataLayer.push(arguments);
-}
+    function gtag() {
+        dataLayer.push(arguments);
+    }
 
-gtag('js', new Date());
-gtag('config', 'G-RCLT2WB2EF');
+    gtag('js', new Date());
+    gtag('config', 'G-RCLT2WB2EF');
 };
